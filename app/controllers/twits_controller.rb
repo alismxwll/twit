d15affc07f68324b519ced065ac9eef963f23367
Aoede Pando & Andrew Akers <student@epicodus.com>
2014-09-12 14:10:13 -0700

    do more stuff than it should.

--- app/controllers/twits_controller.rb
+++ app/controllers/twits_controller.rb
@@ -1,3 +1,38 @@
 class TwitsController < ApplicationController
+  before_filter :authorize
+def index
+    @twits = Twit.all
+  end
 
+  def new
+    @twit = Twit.new
+  end
+
+  def create
+    # @user = User.find(params[:user_id])
+    @twit = Twit.create(twit_params)
+    if @twit.valid?
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: "Twitted!" }
+        format.js
+      end
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @twit = Twit.find(params[:id])
+  end
+
+  def destroy
+    @twit = Twit.find(params[:id])
+    @twit.destroy
+    redirect_to root_path, notice: "Twit Deleted.."
+  end
+
+private
+  def twit_params
+    params.require(:twit).permit(:content, :user_id).merge(:user_id => current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root to: 'twits#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :users
   resources :twits
+  resources :sessions
 end
